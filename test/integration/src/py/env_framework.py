139141d5b14353de3c13070018c81218274565cf
James Moore <james@sifchain.finance>
2021-03-28 17:37:13 -0700

    work in progress

--- test/integration/src/py/env_framework.py
+++ test/integration/src/py/env_framework.py
@@ -1,5 +1,5 @@
+import dataclasses
 import os
-import subprocess
 import sys
 import time
 
@@ -8,6 +8,7 @@ import yaml
 import env_ganache
 import env_geth
 import env_smartcontractrunner
+import env_utilities
 from env_geth import start_geth
 
 configbase = "/configs"
@@ -34,9 +35,7 @@ geth_input = env_geth.GethInput(
     starting_ether=123,
     ws_port=8646,
     http_port=7445,
-    ethereum_addresses="0x0d1d4e623d10f9fba5db95830f7d3839406c6af2,0x0f4f2ac550a1b4e2280d04c21cea7ebd822934b5,0x2191ef87e392377ec08e7c08eb105ef5448eced5,0x2932b7a2355d6fecc4b5c0b6bd44cc31df247a2e,0x5aeda56215b167893e80b4fe645ba6d5bab767de,0x627306090abab3a6e1400e9345bc60c78a8bef57,0x6330a553fc93768f612722bb8c2ec78ac90b3bbc,0x821aea9a577a9b44299b9c15c88cf3087f3b5544,0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef,0xf17f52151ebef6c7334fad080c5704d77216b732".split(
-        ","
-    ),
+    ethereum_addresses=4,
     configoutputfile=config_file_full_path(gethname)
 )
 
@@ -64,7 +63,7 @@ smartcontractrunner_input = env_smartcontractrunner.SmartContractDeployInput(
     configoutputfile=None,
     ws_addr=ganache_ws_addr,
     truffle_network="dynamic",
-    operator_private_key="c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3",
+    operator_private_key=None,
     validator_addresses=["a", "b"],
 )
 
@@ -101,4 +100,14 @@ elif component == "ganache":
 elif component == "smartcontractrunner":
     time.sleep(100000000)
 elif component == "deploy_contracts":
-    env_smartcontractrunner.deploy_contracts(smartcontractrunner_input)
+    ethereum_config = env_utilities.read_json_file(f"{configbase}/ethereum.json")
+    for k in ethereum_config["config"]["private_keys"].keys():
+        i = dataclasses.replace(smartcontractrunner_input, operator_private_key=k)
+        env_smartcontractrunner.deploy_contracts(smartcontractrunner_input)
+        break
+elif component == "deploy_contracts":
+    ethereum_config = env_utilities.read_json_file(f"{configbase}/ethereum.json")
+    for k in ethereum_config["config"]["private_keys"].keys():
+        i = dataclasses.replace(smartcontractrunner_input, operator_private_key=k)
+        env_smartcontractrunner.deploy_contracts(smartcontractrunner_input)
+        break
--- test/integration/src/py/env_geth.py
+++ test/integration/src/py/env_geth.py
@@ -1,11 +1,11 @@
 import subprocess
+import tempfile
 from dataclasses import dataclass
 from typing import List
 
 import env_ethereum
 import env_utilities
 from env_utilities import wait_for_port
-
 
 gethname = "geth"
 
@@ -14,7 +14,7 @@ gethname = "geth"
 class GethInput(env_ethereum.EthereumInput):
     http_port: int
     ws_port: int
-    ethereum_addresses: List[str]
+    ethereum_addresses: int
 
 
 def geth_cmd(args: env_ethereum.EthereumInput) -> str:
@@ -27,31 +27,37 @@ def geth_cmd(args: env_ethereum.EthereumInput) -> str:
         f"--http --http.addr 0.0.0.0 --http.port {args.http_port} --http.api {apis}",
         "--dev --dev.period 1",
         "--mine --miner.threads=1",
-        f"> {args.logfile}",
     ])
     return cmd
 
 
-def fund_initial_accounts(args: env_ethereum.EthereumInput):
+def create_initial_accounts(n: int):
+    return list(map(lambda _: create_account(), range(n)))
+
+
+def fund_initial_accounts(addresses: List[str], starting_amount: int):
     quote = '"'
-    for addr in args.ethereum_addresses:
+    print(f"addresses: {addresses}")
+    for addr in addresses:
         quotedaddr = f"\\{quote}{addr}\\{quote}"
-        cmd = f'geth attach /tmp/gethdata/geth.ipc --exec "eth.sendTransaction({{from:eth.coinbase, to:{quotedaddr}, value:{args.starting_ether * 10 ** 18}}})"'
+        cmd = f'geth attach /tmp/gethdata/geth.ipc --exec "eth.sendTransaction({{from:eth.coinbase, to:{quotedaddr}, value:{starting_amount * 10 ** 18}}})"'
         subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    for addr in args.ethereum_addresses:
+    for addr in addresses:
         quotedaddr = f"\\{quote}{addr}\\{quote}"
         while True:
             cmd = f'geth attach /tmp/gethdata/geth.ipc --exec "eth.getBalance({quotedaddr})"'
             balance_result = subprocess.run(
                 cmd,
                 check=True,
-                text=True, shell=True,
+                text=True,
+                shell=True,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.STDOUT,
                 timeout=10,
             )
+            print(f"getbal: {balance_result}")
             balance = int(float(balance_result.stdout))
-            if balance >= args.starting_ether:
+            if balance >= starting_amount:
                 break;
 
 
@@ -72,8 +78,19 @@ def geth_docker_compose(args: env_ethereum.EthereumInput):
     }
 
 
-def start_geth(args: env_ethereum.EthereumInput):
+def format_new_accounts(accts):
+    result = {}
+    for a in accts:
+        public_address, private_key = a
+        result[public_address] = private_key
+    return {
+        "private_keys": result
+    }
+
+
+def start_geth(args: GethInput):
     """returns an object with a wait() method"""
+    print("starting geth")
     cmd = geth_cmd(args)
     logfile = open(args.logfile, "w")
     proc = subprocess.Popen(
@@ -85,6 +102,47 @@ def start_geth(args: env_ethereum.EthereumInput):
     )
     wait_for_port("localhost", args.ws_port)
     wait_for_port("localhost", args.http_port)
-    fund_initial_accounts(args)
-    env_utilities.startup_complete(args, proc.pid)
+    new_accounts = create_initial_accounts(args.ethereum_addresses)
+    fund_initial_accounts(map(lambda a: a[0], new_accounts), args.starting_ether)
+    env_utilities.startup_complete(args, format_new_accounts(new_accounts))
     return proc
+
+
+def create_account() -> (str, str):
+    """returns a pair of public_address, private_key"""
+    bad_password = "notasecret"
+    with tempfile.NamedTemporaryFile(mode="w", delete=False) as tf:
+        print(bad_password, file=tf, flush=True)
+        cmd = f"geth account new --password {tf.name}"
+        output = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True,
+            check=True,
+            shell=True
+        )
+        s = output.stdout
+        lines = s.split("\n")
+        for l in lines:
+            if "Public address of the key: " in l:
+                _, public = l.split(": ")
+            if "Path of the secret key file: " in l:
+                _, keyfilepath = l.split(": ")
+        print(f"rslt: {output.stdout} | {output.stderr}")
+
+        cmd = f"web3 account extract --keyfile {keyfilepath} --password {bad_password}"
+        output = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True,
+            shell=True
+        )
+        print(f"outputis: {output}")
+        s = output.stdout
+        lines = s.split("\n")
+        for l in lines:
+            if "Private key: " in l:
+                _, private_key = l.split(": ")
+            if "Public address: " in l:
+                _, public_address = l.split(": ")
+        return public_address, private_key
